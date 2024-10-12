89db90f03696beac971d32300b31a1df323dc2cd
jhshin <jhshin@os.korea.ac.kr>
2024-10-13 08:52:36 +0900

    update session control

--- test/2-uds_change_session.py
+++ test/2-uds_change_session.py
@@ -1,6 +1,8 @@
 from doipclient import DoIPClient
-
+import pdb
+import time
 import json
+
 with open("../diag-config.json") as f:
     diag_config = json.loads(f.read())
     #client = DoIPClient("192.168.10.30", 57344, client_logical_address=0x0e80)
@@ -13,4 +15,15 @@ from udsoncan.services import *
 
 uds_connection = DoIPClientUDSConnector(client)
 with Client(uds_connection) as uds_client:
+    pdb.set_trace()
+    print("[Session Change] defaultSession(1) -> ProgrammingSession(2)")
     uds_client.change_session(DiagnosticSessionControl.Session.programmingSession)
+    time.sleep(1)
+
+    print("[Session Change] ProgrammingSession(2) -> extendedDiagnosticSession(3)")
+    uds_client.change_session(DiagnosticSessionControl.Session.extendedDiagnosticSession)
+    time.sleep(1)
+
+    print("[Session Change] extendedDiagnosticSession(3) -> defaultSession(1)")
+    uds_client.change_session(DiagnosticSessionControl.Session.defaultSession)
+    time.sleep(1)
